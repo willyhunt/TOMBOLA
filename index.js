aac854179f481405266446ab9ff4b1eb569b42ac
willyhunt <benjamin.lebotland@gmail.com>
2024-02-05 06:15:20 +0100

    nouveau

---
+++ index.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var btn = document.getElementById('tirageButton');
+    btn.addEventListener('click', function() {
+        alert("Tirage effectué !");
+        // Ici, vous pouvez ajouter la logique pour effectuer le tirage
+    });
+});
+
+// Suppression des fonctions spécifiques à Google Script
+// Vous pouvez ajouter ici des fonctions JavaScript pour gérer le comportement de votre page
+
