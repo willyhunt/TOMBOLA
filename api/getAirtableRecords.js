03ec8314ecf3c0532fc0e8cdf3966a0df7257230
willyhunt <benjamin.lebotland@gmail.com>
2024-02-06 04:56:51 +0100

    env changed

--- api/getAirtableRecords.js
+++ api/getAirtableRecords.js
@@ -9,7 +9,7 @@ module.exports = async (req, res) => {
     };
 
     try {
-        const response = await axios.get(`https://api.airtable.com/v0/${process.env.AIRTABLE_BASE_ID}/${encodeURIComponent(process.env.AIRTABLE_TABLE_NAME)}`, config);
+        const response = await axios.get(`https://api.airtable.com/v0/${process.env.AIRTABLE_BASE_ID}/${encodeURIComponent(process.env.AIRTABLE_TABLE_TIRAGES)}`, config);
         res.status(200).json(response.data);
     } catch (error) {
         console.error('Error fetching Airtable data:', error);
