4e414f1c908b4675518d5c1fd6ed843c39f6c791
willyhunt <benjamin.lebotland@gmail.com>
2024-02-06 06:44:49 +0100

    s

--- api/getAirtableRecords.js
+++ api/getAirtableRecords.js
@@ -10,7 +10,7 @@ module.exports = async (req, res) => {
     };
 
     try {
-        const response = await axios.get(`https://api.airtable.com/v0/${process.env.AIRTABLE_BASE_ID}/${encodeURIComponent(process.env.AIRTABLE_TABLE_TIRAGES)}?filterByFormula=FIND(%22${tirageId}%22,{IDTirage})`, config);
+        const response = await axios.get(`https://api.airtable.com/v0/${process.env.AIRTABLE_BASE_ID}/${encodeURIComponent(process.env.AIRTABLE_TABLE_TICKETS)}?filterByFormula=FIND(%22${tirageId}%22,{IDTirage})`, config);
         res.status(200).json(response.data);
     } catch (error) {
         console.error('Error fetching Airtable data:', error);
