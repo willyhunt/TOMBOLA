40cbb8c7eaf39868b0a6ecdb734eda98a427ba69
willyhunt <benjamin.lebotland@gmail.com>
2024-02-06 06:49:23 +0100

    version fonctionelle 1

--- api/getAirtableRecords.js
+++ api/getAirtableRecords.js
@@ -1,22 +1,16 @@
 const axios = require('axios');
 const tirageId = 'recvZAICq2tq7TFgH';
-const fieldName = 'IDTirage'; // Remplacez par le nom exact du champ
-const formula = `FIND('${tirageId}', {${fieldName}})`;
-
 
 module.exports = async (req, res) => {
     const config = {
         headers: {
             'Authorization': `Bearer ${process.env.AIRTABLE_API_KEY}`,
             'Content-Type': 'application/json'
-        },
-        params: {
-            filterByFormula: encodeURIComponent(formula)
         }
     };
 
     try {
-        const response = await axios.get(`https://api.airtable.com/v0/${process.env.AIRTABLE_BASE_ID}/${encodeURIComponent(process.env.AIRTABLE_TABLE_TICKETS)}`, config);
+        const response = await axios.get(`https://api.airtable.com/v0/${process.env.AIRTABLE_BASE_ID}/${encodeURIComponent(process.env.AIRTABLE_TABLE_TICKETS)}?filterByFormula=FIND(%22${tirageId}%22,{IDTirage})`, config);
         res.status(200).json(response.data);
     } catch (error) {
         console.error('Error fetching Airtable data:', error);
