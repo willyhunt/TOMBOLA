176bc69eb32fe78686f0261eff6a7467f13ea8e0
willyhunt <benjamin.lebotland@gmail.com>
2024-02-06 06:28:24 +0100

    sd

--- api/getAirtableRecords.js
+++ api/getAirtableRecords.js
@@ -11,7 +11,7 @@ module.exports = async (req, res) => {
             'Content-Type': 'application/json'
         },
         params: {
-            filterByFormula: encodeURIComponent(encodedFormula)
+            filterByFormula: encodeURIComponent(formula)
         }
     };
 
