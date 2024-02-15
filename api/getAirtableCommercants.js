80d8ec9e44146512d5efceab5fc883cf06c8b451
willyhunt <benjamin.lebotland@gmail.com>
2024-02-15 19:23:40 +0100

    ajout des logs

--- api/getAirtableCommercants.js
+++ api/getAirtableCommercants.js
@@ -1,13 +1,20 @@
 const axios = require('axios');
+const { Console } = require('console');
 
 module.exports = async (req, res) => {
-    // Récupérer le Annee de la requête, en supposant que vous l'envoyez comme paramètre de requête ou dans le corps de la requête
+    // Log de début de traitement
+    console.log('Début du traitement de la requête.');
+
+    // Récupérer l'année de la requête, en supposant que vous l'envoyez comme paramètre de requête ou dans le corps de la requête
     const Annee = req.query.Annee || (req.body && req.body.Annee);
 
+    // Vérification de la présence de l'année
     if (!Annee) {
+        console.error('Annee is required');
         return res.status(400).json({ error: 'Annee is required' });
     }
 
+    // Configuration de la requête HTTP
     const config = {
         headers: {
             'Authorization': `Bearer ${process.env.AIRTABLE_API_KEY}`,
@@ -16,21 +23,28 @@ module.exports = async (req, res) => {
     };
 
     try {
-        const response = await axios.get(`https://api.airtable.com/v0/${process.env.AIRTABLE_BASE_ID}/${encodeURIComponent(process.env.AIRTABLE_TABLE_COMMERCANTS)}?filterByFormula=FIND(%22${Annee}%22,{Année})`, config);
+        // Log de la requête envoyée à Airtable
+        console.log(`Requête envoyée à Airtable pour l'année: ${Annee}`);
+        
+        const url = `https://api.airtable.com/v0/${process.env.AIRTABLE_BASE_ID}/${encodeURIComponent(process.env.AIRTABLE_TABLE_COMMERCANTS)}?filterByFormula=FIND(%22${Annee}%22,{Année})`;
+        const response = await axios.get(url, config);
         const records = response.data.records;
 
+        // Traitement des données reçues
         const Commercants = records.map(record => ({
-            nomDuCommercant: record.fields["Nom du commerce"] ? record.fields["Nom du commerce"][0] : false
+            nomDuCommercant: record.fields["Nom du commerce"] ? record.fields["Nom du commerce"] : null // Modification pour gérer correctement les noms des commerçants
         }));
 
-        const CommercantsTries = Commercants.sort((a, b) => {
-            return b.nomDuCommercant - a.nomDuCommercant;
-        });
-        
-        
+        // Trier les commerçants par nom
+        const CommercantsTries = Commercants.sort((a, b) => a.nomDuCommercant.localeCompare(b.nomDuCommercant));
 
-        res.status(200).json({ CommercantsTries });
+        // Log de succès
+        console.log('Données récupérées et traitées avec succès.');
+
+        // Réponse avec les données triées
+        res.status(200).json({ Commercants: CommercantsTries });
     } catch (error) {
+        // Log de l'erreur
         console.error('Error fetching Airtable data:', error);
         res.status(500).json({ error: 'Failed to fetch data from Airtable' });
     }
