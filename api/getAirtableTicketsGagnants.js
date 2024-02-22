b82032150a32d87ca0269724742023829592359b
willyhunt <benjamin.lebotland@gmail.com>
2024-02-22 12:57:27 +0100

    Update getAirtableTicketsGagnants.js

--- api/getAirtableTicketsGagnants.js
+++ api/getAirtableTicketsGagnants.js
@@ -20,7 +20,7 @@ module.exports = async (req, res) => {
     try {
         console.log(`Requête envoyée à Airtable pour le tirage: ${tirageId}`);
         
-        const url = `https://api.airtable.com/v0/${process.env.AIRTABLE_BASE_ID}/${encodeURIComponent(process.env.AIRTABLE_TABLE_TICKETS)}?filterByFormula=AND(FIND(%22${tirageId}%22,{IDTirage}), OR({Status} = 'En attente', {Status} = 'Réclamé'))`;
+        const url = `https://api.airtable.com/v0/${process.env.AIRTABLE_BASE_ID}/${encodeURIComponent(process.env.AIRTABLE_TABLE_TICKETS)}?filterByFormula=AND(FIND(%22${tirageId}%22,{IDTirage}), OR({Status} = 'Non réclamé', {Status} = 'Réclamé'))`;
         const response = await axios.get(url, config);
         const records = response.data.records;
 
